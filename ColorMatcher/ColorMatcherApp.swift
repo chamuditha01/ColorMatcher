d05f14725fd257ee65610d8b77f547bf510f64cc
chamuditha <heshanchamuditha05@gmail.com>
2026-01-17 10:02:37 +0530

    Initial Commit

---
+++ ColorMatcher/ColorMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatcherApp.swift
+//  ColorMatcher
+//
+//  Created by COBSCCOMP242P-009 on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorMatcher
+//
+//  Created by COBSCCOMP242P-009 on 2026-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
